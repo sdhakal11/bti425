e483878478e9a65d7b8ce102b9182b3726af4f2a
shaswot dhakal <shaswotdhakal@gmail.com>
2024-01-19 21:02:36 -0500

    Update

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
+/********************************************************************************** 
+WEB422 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+** Name: Shaswot Dhakal
+**Student ID: 114460223
+**Date: 2024 Jan 19
+**Cyclic Link: _______________________________________________________________
+*********************************************************************************/
+
 /*
 id- sdhakal11
 pass- VXGkSImq5T4H8NQA
 */
 
+
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const MoviesDB = require("./modules/moviesDB.js");
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-dotenv.config();
 app.use(cors());
 app.use(express.json());
 
@@ -18,6 +33,17 @@ app.get('/', (req, res) => {
     res.json({message: 'API Listening'});
 });
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});
+const db = new MoviesDB();
+
+db.initialize(process.env.MONGODB_CONN_STRING)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+//mongodb+srv://sdhakal11:VXGkSImq5T4H8NQA@bti425.a6mvs9a.mongodb.net/bti425?retryWrites=true&w=majority
